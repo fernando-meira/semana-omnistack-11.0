d3af106ff3e9dcb267d50872f8d82cbeb885e3bd
Fernando Meira <thomwork@outlook.com>
2020-03-27 00:39:09 -0300

    Initializing the Project

---
+++ backend/src/routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const OngController = require("./Controllers/OngController");
+const IncidentController = require("./Controllers/IncidentController");
+
+const routes = express.Router();
+
+routes.get("/ongs", OngController.index);
+routes.post("/ongs", OngController.create);
+
+routes.post("/incidents", IncidentController.create);
+
+module.exports = routes;
