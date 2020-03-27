75eca19d21615c2f0e0aec0b4cdb1233a3baea5a
Fernando Meira <thomwork@outlook.com>
2020-03-27 01:18:52 -0300

    feat: 🎸 routes(backend)

--- backend/src/Controllers/IncidentController.js
+++ backend/src/Controllers/IncidentController.js
@@ -1,9 +1,15 @@
 const connection = require("../database/connection");
 
 module.exports = {
+  async index(request, response) {
+    const incidents = await connection("incidents").select("*");
+
+    return response.json(incidents);
+  },
+
   async create(request, response) {
     const { title, description, value } = request.body;
-    const ong_id = request.header.authorization;
+    const ong_id = request.headers.authorization;
 
     const [id] = await connection("incidents").insert({
       title,
